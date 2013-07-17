0bd434164e075d6829097f8bbb0797d09183bc1e
Kristofer Rye <kristofer.rye@gmail.com>
2013-07-17 10:55:55 -0400

    doing the scrolling

--- new/public-rss-o-matic/js/rss-o-matic.js
+++ new/public-rss-o-matic/js/rss-o-matic.js
@@ -1,2 +1,7 @@
 $(document).ready (function() {
+	var element_height = document.getElementById('i-cont').offsetHeight;
+
+	console.log(element_height + "\n");
+	
+	$('#i-cont').animate ({'top' : -(element_height - (768 - (64 + 24)))}, 6000, "linear");
 });
