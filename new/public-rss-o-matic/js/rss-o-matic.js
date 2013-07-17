4576c0eef1a929597fa16e24145be89422834d18
Kristofer Rye <kristofer.rye@gmail.com>
2013-07-17 11:07:32 -0400

    constant speed now

--- new/public-rss-o-matic/js/rss-o-matic.js
+++ new/public-rss-o-matic/js/rss-o-matic.js
@@ -1,7 +1,9 @@
 $(document).ready (function() {
 	var element_height = document.getElementById('i-cont').offsetHeight;
 
+
+	
 	console.log(element_height + "\n");
 	
-	$('#i-cont').animate ({'top' : -(element_height - (768 - (64 + 24)))}, 6000, "linear");
+	$('#i-cont').animate ({'top' : -(element_height - (768 - (64 + 24)))}, (element_height / 28) * 1000, "linear");
 });
